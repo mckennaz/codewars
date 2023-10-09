b6e8740bb2b666aab4ced2c078433c2e8527b3a5
mckennaz <133259356+mckennaz@users.noreply.github.com>
2023-10-09 16:28:03 -0400

    Add files via upload

--- codewars/8kyu/codewars/8kyu/script.js
+++ codewars/8kyu/codewars/8kyu/script.js
@@ -1,12 +1,5 @@
-// Wilson primes satisfy the following condition. Let P represent a prime number.
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-// Then,
-
-// ((P-1)! + 1) / (P * P)
-// should give a whole number.
-
-// Your task is to create a function that returns true if the given number is a Wilson prime.
-
-function amIWilson(p) {
-  return p === 5 || p === 13 || p === 563
+function even_or_odd(number) {
+  return number % 2 ? "Odd" : "Even"
 }
