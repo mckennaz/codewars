ba02d53ade67f1f0717d0c69ab8842e41421c91c
mckennaz <133259356+mckennaz@users.noreply.github.com>
2023-10-15 17:14:12 -0400

    Add files via upload

--- 8kyu/script.js
+++ 8kyu/script.js
@@ -1,10 +1,26 @@
-// Given an array of integers, and a target integer, return the largest integer from the array that is less than or equal to the target.
+// The new football league season is coming and the Football Association need some help resetting the league standings. Normally the initial league standing is done in alphabetical order (from A to Z) but this year the FA have decided to freshen it up.
 
-// If there is no number that is less than or equal to the target number then return undefined.
+// It has been decided that team who finished first last season will begin the next season in first place. Regardless of what letter their club begins with. e.g. if Manchester City were in first place last year, they will begin the season in position one. All other teams should be in alphabetical order.
 
-// The array of integers may be empty, but will always be an array.
+// The teams will be fed in as an object ({}). The key will be will be their position from last season and the value is the club's name e.g. Arsenal.
 
-function priceIsRight(numbers, target) {
-  if (target < Math.min(...numbers)) return undefined
-  else return Math.max(...numbers.filter(item => item <= target))
-}
+// The output should be an object ({}) with the key as the club's starting position for the new season and the value should be club's name e.g. Arsenal.
+
+// For example. If in the previous season the standings were:
+
+// 1:'Leeds United' 2:'Liverpool' 3:'Manchester City' 4:'Coventry' 5:'Arsenal'
+
+// Then the new season standings should
+
+// 1:'Leeds United' (first last season) 2:'Arsenal' (alphabetical) 3:'Coventry' (alphabetical) 4:'Liverpool' (alphabetical) 5:'Manchester City' (alphabetical)
+
+function premierLeagueStandings(teamStandings) {
+  // Insert Code here
+  const a =Object.values(teamStandings)
+  const first = a.shift()
+  const arr =[first,...a.sort((a,b)=>a.localeCompare(b))]
+  const obj ={}
+  for(let i=0;i<arr.length;i++) obj[i+1] = arr[i]
+  
+  return obj
+  }
